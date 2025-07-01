f7a7a9a6e95471582892b37599f9e0a7c8862d6a
Steven <steven@ispan.com.tw>
2025-07-01 15:32:45 +0800

    ref()宣告的資料讀值時要加上.value屬性

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AbcComponent from './components/AbcComponent.vue';
+import CountComponent from './components/CountComponent.vue';
 import FirstComponent from './components/FirstComponent.vue';
 
 
@@ -11,6 +12,7 @@ import FirstComponent from './components/FirstComponent.vue';
   <h2>Hello, Vue!!</h2>
   <FirstComponent></FirstComponent>
   <AbcComponent></AbcComponent>
+  <CountComponent></CountComponent>
   <!-- <abc-component></abc-component> -->
 </template>
 
---
+++ src/components/CountComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+    const count = ref(0);
+
+    const add = ()=>{
+        count.value++;
+    }
+
+</script>
+
+<template>
+    <div>
+  <button @click="add">{{ count }}</button>
+    </div>
+</template>
+
+<style lang="css" scoped>
+
+</style>
