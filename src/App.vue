8f744bfbf634e1b295b231c39f5a3fdcbbd075eb
Steven <steven@ispan.com.tw>
2025-07-01 14:48:39 +0800

    第一次Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import FirstComponent from './components/FirstComponent.vue';
+
+
+
+</script>
+
+<template>
+  <h2>Hello, Vue!!</h2>
+  <FirstComponent></FirstComponent>
+</template>
+
+<style scoped></style>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2>第一個 Component</h2>
+</template>
+<script setup></script>
+<style scoped>
+h2 {
+    color: green
+}
+</style>
