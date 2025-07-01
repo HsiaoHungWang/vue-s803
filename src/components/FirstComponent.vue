62939ff0cc21c5161e1333af0caad5f7533b2848
Steven <steven@ispan.com.tw>
2025-07-01 15:24:13 +0800

    {{ }}、v-model 與 ref()

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,7 +1,12 @@
 <template>
-    <h2>第一個 Component</h2>
+    <!--v-model 用在表單元素-->
+    <input type="text" v-model="message" />
+    <h2>{{ message }}</h2>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+const message = ref('第一個 Component...')  //宣告 message 是一個響應式資料
+</script>
 <style scoped>
 h2 {
     color: green
