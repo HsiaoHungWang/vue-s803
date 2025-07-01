aa6564b8430eed8466786c8c6d4c286b52eab7c9
Steven <steven@ispan.com.tw>
2025-07-01 15:15:38 +0800

    onMounted Hook

---
+++ src/components/AbcComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <h2>第二個 Component</h2>
+    <input type="text" id="text1"/>
+</template>
+<script setup>
+import { onBeforeMount, onMounted } from 'vue';
+
+
+onBeforeMount(()=>{
+    console.log('BeforeMount');
+})
+ 
+ //onMounted Hook 發生表示 DOM Tree 產生
+onMounted(()=>{
+    console.log('Mounted');
+ document.querySelector('#text1').focus();
+})
+</script>
+<style scoped>
+h2{color:blue}
+</style>
