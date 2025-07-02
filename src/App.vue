2a5deac10ab1c0ec88be542e1e12ecf123bbcea7
Steven <steven@ispan.com.tw>
2025-07-02 11:04:03 +0800

    Computed Demo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AbcComponent from './components/AbcComponent.vue';
 import BindingComponent from './components/BindingComponent.vue';
+import ComputedDemo from './components/ComputedDemo.vue';
 import CountComponent from './components/CountComponent.vue';
 import EevntBinding from './components/EevntBinding.vue';
 import FirstComponent from './components/FirstComponent.vue';
@@ -12,6 +13,8 @@ import FirstComponent from './components/FirstComponent.vue';
 
 <template>
   <h2>Hello, Vue!!</h2>
+  <ComputedDemo></ComputedDemo>
+  <hr />
   <EevntBinding></EevntBinding>
   <hr />
   <FirstComponent></FirstComponent>
---
+++ src/components/ComputedDemo.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+    const celsius = ref(0);
+
+    const convertToFahrenheit = ()=>{
+        console.log('f')
+        return (celsius.value * 9 / 5) + 32
+    }
+
+    // const fahrenheit = computed(()=>{
+    //     console.log('c')
+    //     return (celsius.value * 9 / 5) + 32
+    // })
+
+     const fahrenheit = computed(()=>(celsius.value * 9 / 5) + 32)
+
+</script>
+
+<template>
+    <div>
+    <label>攝氏：<input type="text" v-model.lazy="celsius" /></label>
+    <p>華氏：{{ (celsius * 9 / 5) + 32  }}</p>
+    <p>華氏：{{ convertToFahrenheit() }}{{ convertToFahrenheit() }}{{ convertToFahrenheit() }}</p>
+    <p>華氏：{{ fahrenheit }}{{ fahrenheit }}{{ fahrenheit }}</p>
+    </div>
+</template>
+
+<style lang="css" scoped>
+
+</style>
