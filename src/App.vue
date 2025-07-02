f910fc3dda44688ea6ba415bb37051f944e92f47
Steven <steven@ispan.com.tw>
2025-07-02 11:36:01 +0800

    計算 BMI

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AbcComponent from './components/AbcComponent.vue';
 import BindingComponent from './components/BindingComponent.vue';
+import BmiComponent from './components/BmiComponent.vue';
 import ComputedDemo from './components/ComputedDemo.vue';
 import CountComponent from './components/CountComponent.vue';
 import EevntBinding from './components/EevntBinding.vue';
@@ -13,6 +14,8 @@ import FirstComponent from './components/FirstComponent.vue';
 
 <template>
   <h2>Hello, Vue!!</h2>
+  <BmiComponent></BmiComponent>
+  <hr />
   <ComputedDemo></ComputedDemo>
   <hr />
   <EevntBinding></EevntBinding>
---
+++ src/components/BmiComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+    const height = ref(170);
+    const weight = ref(70);
+    const bmi = computed(()=>((weight.value/(height.value/100) ** 2)).toFixed(2));
+</script>
+
+<template>
+    <div>
+        <div><label>身高(公分)：<input type="text" v-model.lazy="height" /></label></div>
+        <div><label>體重(公斤)：<input type="text" v-model.lazy="weight" /></label></div>  
+   
+   <p>BMI：{{ bmi }}</p>
+    </div>
+</template>
+
+<style lang="css" scoped>
+
+</style>
