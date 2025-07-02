8015a0bd628dce1e22607e282f04a737dfe344f6
Steven <steven@ispan.com.tw>
2025-07-02 09:45:09 +0800

    v-bind 與 b-model 繫節

--- src/components/BindingComponent.vue
+++ src/components/BindingComponent.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
     console.log(new Date().getDay()) //回傳今天是星期幾 2
     const title = ref('vue')
     const week = ref(["日","一", "二", "三"])
+    
     const author = ref({
         books:[
             'Vue 2 - Advanced Guide',
@@ -14,6 +15,10 @@ import { ref } from 'vue';
     const getName = ()=>{
         return title.value
     }
+
+    const iSpanUrl = ref('https://www.ispan.com.tw')
+    const logoUrl = ref('https://www.ispan.com.tw/_next/image/?url=%2Fimages%2FiSpan_Logo_slogan.png&w=1080&q=75')
+    const isDisabled = ref(true);
 </script>
 
 <template>
@@ -23,8 +28,16 @@ import { ref } from 'vue';
       <p>{{ title }} app {{ 1 + 1 }} is running</p>
       <p>登入帳號 {{ getName() }}</p>
       <p>{{ false ? "yes" : "no" }}</p>
-      <p>今天是星期 {{}}</p>
-      <p>{{}} 書</p>
+      <p>今天是星期 {{ week[new Date().getDay()] }}</p>
+      <p>{{author.books.length===0 ? "沒有" : "有"}} 書</p>
+
+      <h3>屬性繫節 v-bind:屬性</h3>
+      <a v-bind:href="iSpanUrl" :title="title">iSpan</a> | 
+      <a :href="iSpanUrl">iSpan</a>
+      <img :src="logoUrl" />
+
+      <input type="checkbox" v-model="isDisabled">
+      <button :disabled="!isDisabled">click</button>
     </div>
 </template>
 
