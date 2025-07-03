2b758ad905def77c7080071ba918a8cc97531ad9
Steven <steven@ispan.com.tw>
2025-07-03 09:29:34 +0800

    v-show

---
+++ src/components/DirectiveComponent.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+    const data = ref('<u>Hello, World!!</u>')
+    const isLoading = ref(true)
+    setTimeout(()=>{
+        isLoading.value = false;
+    },3000)
+</script>
+
+<template>
+    <div>
+      <div>{{data}}</div>
+      <div v-text="data"></div>
+      <div v-html="data"></div>
+      <img src="/images/loading.gif" v-show="isLoading" />
+      <!-- <img src="/images/dog.webp" v-show="false" />
+      <img src="/images/dog.webp" v-if="false" /> -->
+    </div>
+</template>
+
+<style lang="css" scoped>
+/* img{
+    width:150px
+} */
+</style>
