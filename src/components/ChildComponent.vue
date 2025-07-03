81b2378bf71602da4d1ee19dbd9c19c88709d6b3
Steven <steven@ispan.com.tw>
2025-07-03 14:23:34 +0800

    父到子組件的資料傳遞

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+    const props = defineProps({
+        xyz: String,
+        abc: Number
+    })
+</script>
+
+<template>
+    <div>
+     <h2>Child Component</h2>
+     <p>Hello, {{ xyz }}. You are {{ abc }} years old.</p>
+    </div>
+</template>
+
+<style lang="css" scoped>
+  div{
+    background-color: silver;
+  }
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,10 +1,20 @@
 <script setup>
+import ChildComponent from '@/components/ChildComponent.vue';
+import { ref } from 'vue';
+
+
+const name = ref('Jack')
+const age = ref(30)
     
 </script>
 
 <template>
     <div>
-<h2>Contact Page</h2>
+<h2>Contact Page - Parent</h2>
+<hr />
+<!--透過 xyz 自訂屬性，把父組件中 name 的資料傳給子組件-->
+<!--透過 abc 自訂屬性，把父組件中 age 的資料傳給子組件-->
+<ChildComponent :xyz="name" :abc="age" ></ChildComponent>
     </div>
 </template>
 
