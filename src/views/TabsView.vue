e100896008f20602c6905764044889250ae7a7aa
Steven <steven@ispan.com.tw>
2025-07-03 13:52:32 +0800

    tabs 效果使用 <component>標籤

--- src/views/TabsView.vue
+++ src/views/TabsView.vue
@@ -2,13 +2,31 @@
     import CountComponent from '@/components/CountComponent.vue';
     import FirstComponent from '@/components/FirstComponent.vue';
     import AbcComponent from '@/components/AbcComponent.vue';
+    import { ref } from 'vue';
     const items = [FirstComponent, AbcComponent, CountComponent]
+    const tabs = ref([
+        {"name":"First", "component": FirstComponent},
+        {"name":"Abc", "component": AbcComponent},
+        {"name":"Count", "component": CountComponent},
+    ])
+    const activedIndex = ref(0)
+    const setActive = index =>{
+       activedIndex.value = index
+    }
 </script>
-
 <template>
     <div>
         <h2>Tabs View</h2>
-        <component :is="items[2]"></component>
+        <ul class="nav nav-tabs">     
+            <li class="nav-item" v-for="(tab,idx) in tabs" :key="tab.name">
+                <a class="nav-link" href="#" @mouseover="setActive(idx)">{{tab.name}}</a>
+            </li> 
+        </ul>
+      <div class="mt-3">
+         <component :is="tabs[activedIndex].component"></component>
+      </div>
+
+       
     </div>
 </template>
 
