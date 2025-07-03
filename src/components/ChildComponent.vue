5896b417635fc68ea4bd08c67377fea6c8b269b2
Steven <steven@ispan.com.tw>
2025-07-03 14:53:16 +0800

    子傳父透過事件觸發

--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,14 +1,25 @@
 <script setup>
+//定義父組件傳過來的參數Props
     const props = defineProps({
         xyz: String,
-        abc: Number
+        abc: Number,
+        email: String
     })
+    //定義父組件傳過來的事件
+    const emit = defineEmits(["myEvent"])
+    
+    const clickHandler = ()=>{
+        //觸發父組件傳過來的事件
+        emit("myEvent", "Jack@company.com")
+    }
 </script>
 
 <template>
     <div>
      <h2>Child Component</h2>
      <p>Hello, {{ xyz }}. You are {{ abc }} years old.</p>
+     <p>Email: {{ email }}</p>
+     <button @click="clickHandler">click</button>
     </div>
 </template>
 
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -5,7 +5,12 @@ import { ref } from 'vue';
 
 const name = ref('Jack')
 const age = ref(30)
+const email = ref('Jack@gmail.com')
     
+const myHandler = data => {
+ //data 參數就是子組件傳過來的資料
+ console.log(data);
+}
 </script>
 
 <template>
@@ -14,7 +19,9 @@ const age = ref(30)
 <hr />
 <!--透過 xyz 自訂屬性，把父組件中 name 的資料傳給子組件-->
 <!--透過 abc 自訂屬性，把父組件中 age 的資料傳給子組件-->
-<ChildComponent :xyz="name" :abc="age" ></ChildComponent>
+<!-- myEvent 是自訂事件，把這個事件傳給子組件，讓子組件透過emit()觸發它-->
+<!--myEvent事件觸發後就會執行 myHander 這個函式-->
+<ChildComponent :xyz="name" :abc="age" :email @myEvent="myHandler" ></ChildComponent>
     </div>
 </template>
 
