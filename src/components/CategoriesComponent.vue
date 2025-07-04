efaeccc9abc7d6966b1617ed1991d38c8281e383
Steven <steven@ispan.com.tw>
2025-07-04 11:27:56 +0800

    env 檔的使用

--- src/components/CategoriesComponent.vue
+++ src/components/CategoriesComponent.vue
@@ -1,20 +1,38 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref, watch, watchEffect } from 'vue';
 
-    const apiUrl = 'https://localhost:7247/api/Categories';
+    
     const categories = ref([]);
+
+    const baseApi = import.meta.env.VITE_API_BASEURL;
+    const apiUrl = `${baseApi}/Categories`;
+    // console.log(apiUrl)
 
     // fetch(apiUrl)
     //  .then(response=>response.json())
     //  .then(datas=>categories.value = datas);
-    
-   const loadCategories = async () =>{
-     const response = await fetch(apiUrl);
-     const datas = await response.json();
-     categories.value = datas
-   }
 
-   loadCategories();
+    // watchEffect(async()=>{
+    //   const response = await fetch(apiUrl);
+    //   const datas = await response.json();
+    //   categories.value = datas
+    // })
+
+    //component 產生DOM Tree
+    onMounted(async()=>{       
+       const response = await fetch(apiUrl);
+       const datas = await response.json();
+       categories.value = datas
+    })
+ 
+
+//    const loadCategories = async () =>{
+//      const response = await fetch(apiUrl);
+//      const datas = await response.json();
+//      categories.value = datas
+//    }
+
+//    loadCategories();
     
 
      
