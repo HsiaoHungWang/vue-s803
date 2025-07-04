965e2238479013fef109be62ec789f03ab6cee17
Steven <steven@ispan.com.tw>
2025-07-04 10:57:45 +0800

    改成 async await

--- src/components/CategoriesComponent.vue
+++ src/components/CategoriesComponent.vue
@@ -4,9 +4,19 @@ import { ref } from 'vue';
     const apiUrl = 'https://localhost:7247/api/Categories';
     const categories = ref([]);
 
-    fetch(apiUrl)
-     .then(response=>response.json())
-     .then(datas=>categories.value = datas);
+    // fetch(apiUrl)
+    //  .then(response=>response.json())
+    //  .then(datas=>categories.value = datas);
+   const loadCategories = async () =>{
+     const response = await fetch(apiUrl);
+     const datas = await response.json();
+     categories.value = datas
+   }
+
+   loadCategories();
+    
+
+     
      
 </script>
 
